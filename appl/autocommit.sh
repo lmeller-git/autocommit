81fc9dc2469a70ed853c69ee39db6d1a0722dfc2
lmeller-git <louis.meller@icloud.com>
2025-02-09 18:54:33 +0100

    added qol scripts

---
+++ appl/autocommit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname "$(dirname "${BASH_SOURCE[0]}")")
+INSTALL_SCRIPT="$BASE_DIR/scripts/init.sh"
+PYTHON_SCRIPT="$BASE_DIR/message_gen/src/main.py"
+
+echo "$BASE_DIR"
+
+if [[ $# -eq 1 && -d "$1" ]]; then
+    "$INSTALL_SCRIPT" "$1"
+    exit 0
+fi
+
+
+VENV_DIR="$BASE_DIR/venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Error: Virtual environment not found in autocommit directory. Please run install.sh again." >&2
+    exit 1
+fi
+
+PYTHON="$VENV_DIR/bin/python"b
+
+if [[ "$1" == "--diff" && -n "$2" && "$3" == "--old" && -n "$4" ]]; then
+    python "$PYTHON_SCRIPT" "$2" "$4"
+    exit 0
+fi
+
+echo "Usage:"
+echo "  autocommit <repo_dir>  # Install hooks in the specified repo"
+echo "  autocommit --diff <diff_file> --old <old_message>  # Generate a commit message"
+exit 1
--- scripts/init.sh
+++ scripts/init.sh
@@ -8,19 +8,22 @@ fi
 TARGET_REPO="$1"
 HOOKS_DIR="$TARGET_REPO/.git/hooks"
 HOOK_FILE="$HOOKS_DIR/commit-msg"
-AUTOCOMMIT_DIR="$HOME/autocommit"
-AUTOCOMMIT_DIR="$(dirname "$0")"
+AUTOCOMMIT_DIR=$(dirname "$(dirname "$0")")
 VENV_DIR="$AUTOCOMMIT_DIR/venv"
+
+echo "$TARGET_REPO", "$HOOKS_DIR", "$HOOK_FILE", "$AUTOCOMMIT_DIR", "$VENV_DIR" 
+# exit 0
 
 if [ ! -d "$TARGET_REPO/.git" ]; then
     echo "Error: '$TARGET_REPO' is not a valid Git repository."
     exit 1
 fi
 
-echo "Installing commit-msg hook in $TARGET_REPO..."
+echo "Installing commit-msg hook in $HOOKS_DIR"
 
+# exit 0
 # mkdir -p "$HOOKS_DIR"
-cp "$AUTOCOMMIT_DIR/hooks/commit-msg" "$HOOK_FILE"
+cp "$AUTOCOMMIT_DIR/scripts/commit-msg" "$HOOK_FILE"
 chmod +x "$HOOK_FILE"
 
 if [ ! -d "$VENV_DIR" ]; then
@@ -32,4 +35,4 @@ fi
 echo "Installing dependencies in virtual environment..."
 "$VENV_DIR/bin/pip" install -r "$AUTOCOMMIT_DIR/requirements.txt"
 
-echo "Installation complete. The commit-msg hook is now active for $TARGET_REPO."
+echo "Installation complete. The commit-msg hook is now active for $TARGET_REPO
